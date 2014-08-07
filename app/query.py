3d217b3d6b40a4283360a1ba0e0af017b30d479f
Matt Parrilla <matthew.parrilla@gmail.com>
2014-08-07 12:17:59 -0400

    Style band detail

--- app/query.py
+++ app/query.py
@@ -33,6 +33,13 @@ def get_bands():
         band['youtubelink'] = '%sembed/%s?rel=0&start=0' % tuple(
             band['youtubelink'].rsplit('watch?v='))
         band['slug'] = slugify(band['bandname'])
+        band['bandmembers'] = [member for member in band['bandmembers'].split(';')
+            if member]
+        for i, member in enumerate(band['bandmembers']):
+            member_list = member.split(',')
+            band['bandmembers'][i] = {'name': member_list[0],
+                'instrument': member_list[1]}
+        band['setlist'] = band['setlist'].split(';')
     sheet.reverse()
 
     return sheet
