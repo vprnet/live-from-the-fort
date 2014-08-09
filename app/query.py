187c94f5a5251bda3888d81d6e2c820168ad0502
Matt Parrilla <matthew.parrilla@gmail.com>
2014-08-09 15:33:03 -0400

    Complete project design version 0

--- app/query.py
+++ app/query.py
@@ -29,17 +29,21 @@ def get_google_sheet(sheet_key=False, sheet_id='od6'):
 def get_bands():
     """Takes google sheet object, modifies some fields, puts newest first"""
     sheet = get_google_sheet(GOOGLE_SPREADSHEET['KEY'])
-    for band in sheet:
-        band['youtubelink'] = '%sembed/%s?rel=0&start=0' % tuple(
-            band['youtubelink'].rsplit('watch?v='))
-        band['slug'] = slugify(band['bandname'])
-        band['bandmembers'] = [member for member in band['bandmembers'].split(';')
-            if member]
-        for i, member in enumerate(band['bandmembers']):
-            member_list = member.split(',')
-            band['bandmembers'][i] = {'name': member_list[0],
-                'instrument': member_list[1]}
-        band['setlist'] = band['setlist'].split(';')
-    sheet.reverse()
+    band_list = []
+    for i, band in enumerate(sheet):
+        if band['youtubelink']:
+            band['youtubelink'] = '%sembed/%s?rel=0&start=0' % tuple(
+                band['youtubelink'].rsplit('watch?v='))
+            band['slug'] = slugify(band['bandname'])
+            band['bandmembers'] = [member for member in band['bandmembers'].split(';')
+                if member]
+            for j, member in enumerate(band['bandmembers']):
+                member_list = member.split(',')
+                band['bandmembers'][j] = {'name': member_list[0],
+                    'instrument': member_list[1]}
+            band['setlist'] = band['setlist'].split(';')
+            band_list.insert(0, band)
+        else:
+            band_list.append(band)
 
-    return sheet
+    return band_list
--- app/views.py
+++ app/views.py
@@ -3,36 +3,40 @@ from flask import render_template, request
 from query import get_bands
 from config import BASE_URL
 
+bands = get_bands()
+
+project_social = {
+    'url': BASE_URL,
+    'title': "Live From The Fort",
+    'subtitle': "",
+    'img': bands[0]['coverphoto'],
+    'description': "'Live From The Fort' is an online video music series by Vermont Public Radio. The intimate performances, recorded at VPR studios in Colchester's historic Fort Ethan Allen, provide an opportunity to discover and explore the rich and diverse contemporary music scene Vermont.",
+    'twitter_text': "Intimate performances by Vermont's own music makers.",
+    'twitter_hashtag': "LiveFromTheFort"
+}
+
 
 @app.route('/')
 def index():
     page_url = BASE_URL + request.path
     page_title = 'Live From The Fort'
-    bands = get_bands()
     landing = True
 
-    social = {
-        'title': "Live From The Fort",
-        'subtitle': "",
-        'img': "",
-        'description': "",
-        'twitter_text': "",
-        'twitter_hashtag': ""
-    }
+    social = project_social
 
     return render_template('content.html',
         page_title=page_title,
         social=social,
         bands=bands,
         landing=landing,
+        project_social=project_social,
         page_url=page_url)
 
 
 @app.route('/<bandname>')
 def band_page(bandname):
-    bands = get_bands()
     for band in bands:
-        if bandname == band['slug']:
+        if 'slug' in band and bandname == band['slug']:
             bands.remove(band)
             bands.insert(0, band)
 
@@ -40,16 +44,18 @@ def band_page(bandname):
     page_title = 'Live From The Fort: ' + bands[0]['bandname']
 
     social = {
-        'title': "Live From The Fort",
-        'subtitle': "",
-        'img': "",
-        'description': "",
-        'twitter_text': "",
-        'twitter_hashtag': ""
+        'title': page_title,
+        'subtitle': "Recorded at VPR Studios",
+        'img': bands[0]['coverphoto'],
+        'description': "In this month's installment of 'Live From The Fort' " +
+        bands[0]['bandname'] + ' performs at historic Fort Ethan Allen',
+        'twitter_text': bands[0]['bandname'] + " performs at VPR Studios",
+        'twitter_hashtag': "LiveFromTheFort"
     }
 
     return render_template('content.html',
         page_title=page_title,
         social=social,
         bands=bands,
+        project_social=project_social,
         page_url=page_url)
